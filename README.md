ffa52ddc2385a52391bfa875d0224102b8fad2eb
lizwachira <lizwanguwachira@gmail.com>
2024-10-23 22:04:41 +0300

    Scraping with Py

---
+++ README.md
@@ -0,0 +1,8 @@
+### Command Run
+python scrape.py
+
+The output you’re seeing is the HTML code of the website.
+
+### terminal
+git init
+git add .
---
+++ scrape.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+
+
+url = "http://olympus.realpython.org/profiles/aphrodite"
+page = urlopen(url)
+
+page
+# <http.client.HTTPResponse object at 0x105fef820>
+html_bytes = page.read()
+
+html = html_bytes.decode("utf-8")
+print(html)
